6a2f5becf71839e5e95c1ce7215e0faf45b9a874
szl <937771581@qq.com>
2019-12-08 10:09:09 +0800

    123

--- src/serviceAPI.config.js
+++ src/serviceAPI.config.js
@@ -5,31 +5,8 @@ if(process.env.NODE_ENV === 'development'){
 }else if(process.env.NODE_ENV === 'test'){
     var LOCALURL =  "http://www.website.cc/"
 }else{
-    var LOCALURL =  ""
-    var host = location.host
-
-     if(host==='fleekfly.xyz'){
-         LOCALURL =  "http://shop.fleekfly.xyz/"
-     }else if(host==='www.global-shop-id.com'){
-         LOCALURL =  "http://globalshop.fleekfly.xyz/"
-     }else if(host==='www.perbelanjaan-id.com'){
-        LOCALURL =  "http://perbelanjaan.fleekfly.xyz/"
-    }else if(host==='www.crystalmarket-id.com'){
-        LOCALURL =  "http://crystalmarket.fleekfly.xyz/"
-    }else if(host==='www.daily-shopping-mall.com'){
-        LOCALURL =  "http://dailyshop.fleekfly.xyz/"
-    }else if(host==='www.richard-market.com'){
-        LOCALURL =  "http://richardmarket.fleekfly.xyz/"
-    }else if(host==='www.watchmall-id.com'){
-        LOCALURL =  "http://watchmall.fleekfly.xyz/"
-    }else if(host==='www.crystalmarket.sg'){
-        LOCALURL =  "http://crystalsg.fleekfly.xyz/"
-    }
-    else if(host==='www.joyelikeyoga.com'){
-        LOCALURL =  "http://joyelikeyoga.fleekfly.xyz/"
-    }else{
-        LOCALURL =  "http://shop.fleekfly.xyz/"
-    }
+    LOCALURL =  "http://shop.fleekfly.xyz/"
+     
 }
 console.log(process.env.NODE_ENV,location.host)
 const URL ={
